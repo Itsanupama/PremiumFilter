d915e7a5f419ca9a98d16eca16f537089bf7a403
Anupamafan <118895531+Anupamafan@users.noreply.github.com>
2023-09-08 22:04:06 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -784,10 +784,9 @@ async def auto_filter(client, msg, spoll=False):
         btn.append(
             [InlineKeyboardButton(text="🗓 1/1", callback_data="pages")]
         )
-    buttons = [ [
+    btn.insert(0, [
         InlineKeyboardButton("😌 Cʜᴇᴄᴋ Bᴏᴛ PM 😌", url=f"https://t.me/{temp.U_NAME}")
-        InlineKeyboardButton("😌 🔰ʜᴏᴡ ᴛᴏ ᴅᴏᴡɴʟᴏᴀᴅ🔰 😌", url=f"https://t.me/How_To_Open_shortLinks/19")
-    ] ]
+    ])
     imdb = await get_poster(search, file=(files[0]).file_name) if settings["imdb"] else None
     TEMPLATE = settings['template']
     if imdb:
